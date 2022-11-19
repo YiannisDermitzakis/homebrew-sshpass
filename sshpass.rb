363e7eb930c1cbe6bb281706b4fbfa6cb28690d3
Yiannis Dermitzakis <DermitzakisYiannis@gmail.com>
2022-11-19 16:26:30 +0200

    Update hash type

--- sshpass.rb
+++ sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
-  sha1 "cbc0e1300c7f1cacf9c850e5b42773cb53211d9d"
+  md5 "191a9128a74d81ae36744d5deb50d164"
 
   def install
     system "./configure", "--disable-debug",
