53ac20a4b511ea2e81f05a951d4e739be455a1a5
Yiannis Dermitzakis <DermitzakisYiannis@gmail.com>
2022-11-19 16:31:18 +0200

    Use sha256 (others were invalid)

--- sshpass.rb
+++ sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
-  md5 "191a9128a74d81ae36744d5deb50d164"
+  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
 
   def install
     system "./configure", "--disable-debug",
